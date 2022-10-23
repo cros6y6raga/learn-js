0e3a5404267e86dd7b1ca79a0703c3296edf23fe
cros6y6raga <crosbybraga@gmail.com>
2022-10-23 20:41:41 +0300

    2

--- main.js
+++ main.js
@@ -7,3 +7,77 @@
 // let eart;
 // let nameuser;
 
+// let name=prompt('Ваше имя?','Владимир')
+// alert(`Ваше имя ${name}`)
+
+// let a = +prompt("Первое число?", 1);
+// let b = +prompt("Второе число?", 2);
+//
+// alert(a + b); // 12
+
+// let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?', '');
+//
+// if (year == 2015) {
+//     alert('Вы правы!');
+// } else {
+//     alert('Не верно!');
+// }
+
+// let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?', '');
+//
+// if (year == 2015) {
+//     alert( 'Да вы знаток!' );
+// } else {
+//     alert( 'А вот и неправильно!' ); // любое значение, кроме 2015
+// }
+// let age = prompt('Сколько вам лет?', '');
+// let accessAllowed = age > 18;
+// alert(accessAllowed);
+
+// if ("0") {
+//     alert( 'Привет' );
+// }
+
+// let name = prompt('Какое «официальное» название JavaScript?')
+// if (name==='ECMAScript') {
+//     alert('Верно!');
+// }
+// else {
+//     alert('Не знаете? ECMAScript!');
+// }
+
+// let value = prompt('Выведет число', 0)
+// if (value > 0) {
+//     alert(1)
+// }
+// if (value < 0) {
+//     alert(-1)
+// }
+// else if (value == 0) {
+//     alert(0)
+// }
+
+// let result;
+//
+// if (a + b < 4) {
+//     result = 'Мало';
+// } else {
+//     result = 'Много';
+// }
+
+// let result = a + b < 4 ? 'Мало' : 'Много';
+// alert(result)
+
+// let message;
+//
+// if (login == 'Сотрудник') {
+//     message = 'Привет';
+// } else if (login == 'Директор') {
+//     message = 'Здравствуйте';
+// } else if (login == '') {
+//     message = 'Нет логина';
+// } else {
+//     message = '';
+// }
+
+// let message = (login == 'Сотрудник') ? 'Привет' : (login == 'Директор') ? 'Здравствуйте' : (login == '') ? 'Нет логина' : '';
